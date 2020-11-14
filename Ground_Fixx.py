0e44179083808febb5bd3897d146e3b84cf388dc
RathHunt <asda@affwa>
2020-11-14 00:26:51 -0500

    +choose location to scan

--- Ground_Fixx.py
+++ Ground_Fixx.py
@@ -63,25 +63,28 @@ if __name__ == '__main__':
     print ('\n\tGround_fixx by RathHunt :V')
     print ('\t'+'*'*26+'\n')
 
-    path = '.'
+    path = input('Enter locations to scan separared by spaces exmaple[E:/ D:/work/]\n')
+
+    path = path.split(' ')
 
     modify= input('Fix? (1 for fix / 2 for just list)\n')
-    print ('Scanning ' + os.getcwd() + '...')
+
     kernel32 = WinDLL('kernel32', use_last_error=True)
 
     cont = False
-
-    for entry in scantree(path):
-        if entry.is_file() and entry.name.startswith('g') and entry.name.endswith('.exe'):
-            a,s,h,r,i = myattrib(kernel32, entry)
-            #print(entry.path, a,s,h,r,i)
-            if s==1 and h==1:
-                cont = True
-                print(entry.path)
-                if(modify == '1'):
-                    print('\t' + entry.path + ' -> ' + entry.path[:-len(entry.name)] + entry.name [1:])
-                    myattrib(kernel32, entry, a=None, s=False, h=False, r=None, i=None)
-                    shutil.move(entry.path, entry.path[:-len(entry.name)] + entry.name [1:])
+    for location in path:
+        print ('Scanning ' + location + '...')
+        for entry in scantree(location):
+            if entry.is_file() and entry.name.startswith('g') and entry.name.endswith('.exe'):
+                a,s,h,r,i = myattrib(kernel32, entry)
+                #print(entry.path, a,s,h,r,i)
+                if s==1 and h==1:
+                    cont = True
+                    print(entry.path)
+                    if(modify == '1'):
+                        print('\t' + entry.path + ' -> ' + entry.path[:-len(entry.name)] + entry.name [1:])
+                        myattrib(kernel32, entry, a=None, s=False, h=False, r=None, i=None)
+                        shutil.move(entry.path, entry.path[:-len(entry.name)] + entry.name [1:])
     if not cont:
         print("Nothing Found")
 
